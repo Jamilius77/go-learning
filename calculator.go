963ba78a1834907cf225bf2b409574fafdee4bd6
Jamilius <gadzhiev.jamil97@gmail.com>
2025-05-03 22:08:52 +0300

    добавил вызов мейн в каждую функцию

--- calculator.go
+++ calculator.go
@@ -28,16 +28,24 @@ func main() {
 func add(a int, b int) {
 	var result int = a + b
 	fmt.Println("ответ", result)
+	main()
 }
 func sub(a int, b int) {
 	var result int = a - b
 	fmt.Println("результат", result)
+	main()
 }
 func multiply(a int, b int) {
 	var result int = a * b
 	fmt.Println("Результат", result)
+	main()
 }
 func split(a int, b int) {
+	if b == 0 {
+		fmt.Println("деление на ноль ошибка")
+		main()
+	}
 	var result int = a / b
 	fmt.Println("Ответ", result)
+	main()
 }
