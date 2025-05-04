d66be2877d2e52139bdd4827cc8c1b80069a392f
Jamilius <gadzhiev.jamil97@gmail.com>
2025-05-04 04:50:37 +0300

    проверка числа на четное, нечетное

---
+++ checkintger.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numbers = [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	for _, value := range numbers {
+		checkintger(value)
+
+	}
+}
+func checkintger(a int) {
+	if a%2 == 0 {
+		fmt.Println("четное", a)
+	} else {
+		fmt.Println("нечетное", a)
+	}
+
+}
