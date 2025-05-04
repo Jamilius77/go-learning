230fa9a0ce8e53a9bc974167a619d128b69d8f5a
Jamilius <gadzhiev.jamil97@gmail.com>
2025-05-04 21:18:39 +0300

    вернул файл назад

--- findMaxNumber.go
+++ findMaxNumber.go
@@ -1,6 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
-v
+	var numbers = [5]int{99, 24, 645, 22, 12}
+	findMaxNumber(numbers)
 }
-func
+func findMaxNumber(array [5]int) {
+	var max int = 0
+	for _, value := range array {
+		if value > max {
+			max = value
+		}
+	}
+	fmt.Println("Самое большое число", max)
+}
